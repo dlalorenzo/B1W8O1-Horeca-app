692b295acf23309f48a478fc9f59e820a6f32be0
lorenzo <99051451@mydavinci.nl>
2018-10-27 12:15:54 +0200

    https://github.com/dlalorenzo/B1W8O1-Horeca-app

---
+++ JS/horecaLogic.js
@@ -0,0 +1,23 @@
+
+const BIER=2,75;
+const WIJN=3,75;
+const FRIS=2;
+const SNACKS=2,50;
+
+var bier,wijn,fris;
+var qeustion='wat wilt u drinken\n1)bier &euro;2,75\n2)wijn &euro;3,75\n3)fris &euro;2';
+var qeustionFood='wil je 8 of 16 stuks bitterbalen';
+
+prompt(qeustion+qeustionFood)
+
+if(qeustion == bier){
+	alert('hvl wil je')
+
+}else if(qeustion == wijn){
+	alert('hvl wil je')
+
+}else if(qeustion == bier){
+	alert('hvl wil je')
+}else{
+	prompt(qeustion)
+}
